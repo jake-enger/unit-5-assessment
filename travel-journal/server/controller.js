48ed0790e509a63a8d83d3ab67fc5e7758a60817
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-05-02 18:29:12 -0600

    added data table PDF

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,5 +1,14 @@
 const Sequelize = require('dotenv').config()
 
+const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized : false
+        }
+    }
+})
+
 
 module.exports = {
     seed: (req, res) => {
